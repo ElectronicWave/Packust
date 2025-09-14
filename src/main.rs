4c523941070afd87a4725fa8b5cfad4ea806c4af
oganesson897 <oganesson897@gmail.com>
2025-09-14 09:17:31 +0900

    chore: sth

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Init {},
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Args::parse();
+
+    match &cli.command {
+        Some(Commands::Init {}) => {
+            println!("Initializing...");
+        }
+        None => {
+            println!("No command provided. Use --help for more information.");
+        }
+    }
 }
